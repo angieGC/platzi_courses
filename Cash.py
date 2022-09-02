58f9f63c3cab35686d74ec2e654b72b725ce0a3a
Angie Garcia <angiecata.30@gmail.com>
2022-09-02 10:33:38 -0500

    changes

--- Cash.py
+++
@@ -1,6 +0,0 @@
-from payment import Payment
-
-class Cash(Payment):
-    def __init__(self, id):
-      super.__init__(id)
-     
--- route.py
+++
@@ -1,7 +0,0 @@
-from curses import start_color
-
-
-class Route :
-    id  = int
-    start = []
-    end = []
